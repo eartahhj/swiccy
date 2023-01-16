e915bb38a606aa3053d901f28830f0b4503f2a10
eartahhj <eartahhj@gaminghouse.community>
2023-01-16 14:34:28 +0100

    Update gettext_helper.php

--- app/Helpers/gettext_helper.php
+++ app/Helpers/gettext_helper.php
@@ -4,7 +4,23 @@ if (!function_exists('loadGettext')) {
     function loadGettext(string $language)
     {
         $domain = 'swiccy_' . $language;
-        setlocale(LC_ALL, $language);
+
+        if (defined('LC_ALL')) {
+            setlocale(LC_ALL, $language);
+        }
+
+        if (defined('LC_CTYPE')) {
+            setlocale(LC_CTYPE, $language);
+        }
+
+        if (defined('LC_MESSAGES')) {
+            setlocale(LC_MESSAGES, $language);
+        }
+
+        if (defined('LC_TIME')) {
+            setlocale(LC_TIME, $language);
+        }
+
         bindtextdomain($domain, APPPATH . 'Language/locale');
         textdomain($domain);
         bind_textdomain_codeset($domain, 'UTF-8');
